4b71939371f89b83f602700e2a869da8b43b2e04
Parawee921 <weawanpoaphang@gmail.com>
2024-12-02 11:41:32 +0700

    second commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/my_contrainer.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter Simple',
+      home: MyContrainer()
     );
   }
 }
---
+++ flutter_application_1/lib/my_contrainer.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MyContrainer extends StatelessWidget {
+  const MyContrainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Using Contrainer',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFFB00020),
+        centerTitle: true,
+      ),
+      body: Center(
+       child: Column(
+        children: [
+          contrainer1(),
+          contrainer2(),
+        ],
+
+       ),
+      )
+      );
+  }
+  Widget contrainer2() => Container(
+    height: 250,width: 300,
+    margin: const EdgeInsets.all(50),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(
+        color: Colors.black,
+        width: 2
+      ),
+      shape: BoxShape.rectangle,
+      borderRadius: const BorderRadius.all(Radius.elliptical(10, 10))
+    ),
+    //color: Colors.black87,
+    child: const Center(
+    child: Text('L O V E',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+    
+  ),
+  );
+  Widget contrainer1() => Container(
+     height: 250,
+        width: 300,
+        margin: const EdgeInsets.all(30),
+        decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(
+        color: Colors.black,
+        width: 2
+      ),
+      shape: BoxShape.rectangle,
+      borderRadius: const BorderRadius.all(Radius.elliptical(10, 10))
+    ),
+        //color: Colors.white,
+        child: const Center(
+        child:  Text('I am Rich',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 25),
+        )
+      ),
+  );
+}
